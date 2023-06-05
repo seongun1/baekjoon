a2fc0c4b8d3749b4d670f80d1d5b3839051f4cac
김성운 <115334493+seongun1@users.noreply.github.com>
2023-06-05 20:30:09 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -9,17 +9,20 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer(id_list.size(), 0);
 
     map<string, int> id_map;
-    map<string, set<string>> report_count;
+    map<string, set<string>> report_count; // 신고 받은 한 사람과 그 사람을 신고한 사람들을 저장하는 map
     for (int i = 0; i < id_list.size(); i++) {
         id_map[id_list[i]] = i;
     }
-
+    // report 에서는 신고 한사람 -> 신고 받은 사람으로 데이터가 들어온다.
+    // 신고 당한 한 사람과 / 신고를 한 사람들을 한 사람, 그 사람을 신고한 배열들로 파싱하여 report_count에 담는다.
         for (auto rep : report) {
             stringstream ss(rep);
             string from, to;
             ss >> from >> to;
             report_count[to].insert(from);
         }
+        //만약 신고를 한 사람들의 숫자가 일정 숫자(k)가 넘어가면, 신고를 받아서 정지를 하고, 
+        //id_map(회원 리스트)의 있는 사람들을 검색하여(id_map[in_iter]) 그 사람이 받는 메일의 갯수(answer)의 값을 증가 시킨다.
         for (auto iter : report_count) {
             if (iter.second.size() >= k) {
                 for (auto in_iter : iter.second) {
@@ -27,6 +30,5 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k) {
                 }
             }
         }
-    
     return answer;
 }
