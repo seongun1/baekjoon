fbd81e67738c5ae86f3a4c11d9b513bfb0f85fd1
김성운 <115334493+seongun1@users.noreply.github.com>
2023-07-01 18:19:04 +0900

    [Silver IV] Title: 제로, Time: 76 ms, Memory: 32036 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+result = []
+for i in range (k):
+    num = int(input())
+    if num == 0:
+        result.pop()
+    else:
+        result.append(num)
+print(sum(result))
