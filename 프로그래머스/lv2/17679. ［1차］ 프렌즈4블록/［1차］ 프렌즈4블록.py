3851085c6a331b8c031227cd9e5dd1d65b6c4bd5
김성운 <115334493+seongun1@users.noreply.github.com>
2023-08-16 12:26:14 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 2.62 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,42 +1,44 @@
 def check(m, n, board):
-    graph =[[0 for _ in range(n)]for _ in range(m)]
+    graph =[[0 for _ in range(n)]for _ in range(m)] #높이 m 폭 n
     count = 0
-    
-    for i in range(m-1):
-        for j in range(n-1):
-            a= board[i][j]
-            b=board[i][j+1]
-            c=board[i+1][j]
-            d = board[i+1][j+1]
-            if a== b==c==d and a !='0':
-                graph[i][j],graph[i+1][j],graph[i][j+1],graph[i+1][j+1] = 1,1,1,1
-                
-    for i in range(m):
-        for j in range(n):
-            if graph[i][j] ==1:
-                count +=1
-                board[i][j] ='0'
-                
-    if count ==0:
-        return 0
+    while True:
+        fflag =True
+        for i in range(m-1):
+            for j in range(n-1):
+                a= board[i][j]
+                b= board[i][j+1]
+                c= board[i+1][j]
+                d= board[i+1][j+1]
+                if a== b==c==d and a !='0':
+                    graph[i][j],graph[i+1][j],graph[i][j+1],graph[i+1][j+1] = 1,1,1,1
+                    fflag = False
+              
+        for i in range(m):
+            for j in range(n):
+                if graph[i][j] ==1:
+                    graph[i][j] =0
+                    count +=1
+                    board[i][j] ='0'
+        if fflag:
+            break 
+
     #터진 부분 채우기
-    for i in range(m-2,-1,-1):
-        for j in range(n):
-            k=i
-            while 0<= k+1 < m and board[k+1][j] =='0':
-                k+=1
-            if k !=i:
-                board[k][j] = board[i][j]
-                board[i][j] ='0'
-                
+        while True:
+            flag = True
+            for i in range(m-1):
+                for j in range(n):
+                    if board[i+1][j] =='0' and board[i][j] != '0':
+                        board[i+1][j] = board[i][j]
+                        board[i][j] ='0'
+                        flag = False
+            if flag:
+                break
+
     return count
+
+    
 def solution(m,n,board):
-    answer =0
     board = list(map(list,board))
     
-    while True:
-        tmp = check(m,n,board)
-        if tmp ==0:
-            break
-        answer += tmp
-    return answer
+    tmp = check(m,n,board)
+    return tmp
