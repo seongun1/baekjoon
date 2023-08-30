15b4668bd54bd4f22a29ee356d75633a717dc5b9
김성운 <115334493+seongun1@users.noreply.github.com>
2023-08-30 22:46:39 +0900

    [Gold V] Title: 리모컨, Time: 1136 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # 이동하려고 하는 채널
+m = int(input()) # 고장난 버튼의 개수
+if m:
+    broken = set(input().split())
+else:
+    broken = set()
+remote = [i for i in range(10)]
+
+standard = abs(n-100) # 최댓값
+
+for num in range(1000001):
+    for j in str(num):
+        if j in broken:
+            break
+    else:
+        standard = min(standard,len(str(num)) + abs(num - n))
+print(standard)
