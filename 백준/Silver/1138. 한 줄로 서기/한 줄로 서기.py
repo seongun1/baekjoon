bc0469fcf65bc68bbacf71e8251a6e56c44aa256
김성운 <115334493+seongun1@users.noreply.github.com>
2024-10-28 14:57:34 +0900

    [Silver II] Title: 한 줄로 서기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int,input().split()))
+ans = [n+1 for _ in range(n)]
+for i,a in enumerate(arr):
+    count=0
+    for j in range(n):
+        if ans[j] == n+1:
+            count +=1
+            if count >a:
+                ans[j] = i+1
+                break
+            
+print(' '.join(map(str,ans)))
