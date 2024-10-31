845ca4fbe33bf7b8c5c7a25878890e324f82b9d2
김성운 <115334493+seongun1@users.noreply.github.com>
2024-10-31 16:55:18 +0900

    [Silver II] Title: A → B, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,25 +1,19 @@
 import sys
-
 input = sys.stdin.readline
 
 a,b = map(int,input().split())
 def count(a,b):
     count=1
     while(a!=b):
-        if b==1:
+        if b ==0:
             return -1
         count +=1
-        last_digit = b%10
-        if last_digit ==1:
-            b = str(b)
-            b = list(b)
-            b = b[:len(b)-1]
-            b = ''.join(b)
-            b = int(b)
+        if not b%2:
+            b //=2
+        elif b%10 ==1:
+            b //=10
             #print(b)
-        elif not last_digit%2:
-            b//=2
-        else: # 1이 아닌 홀수인 경우
+        else:
             return -1
     return count
 
