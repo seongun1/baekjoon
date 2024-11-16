a513df5f2355073ec9f4b1f0f1c1c6c0843e47df
김성운 <115334493+seongun1@users.noreply.github.com>
2024-11-16 22:00:35 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 444 ms, Memory: 41660 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,58 +1,56 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
-n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-# 초기화
+from collections import deque
+n,m = map(int,input().split())
+arr =[list(map(int,input().split())) for _ in range(n)]
+#print(arr)
 MAXSIZE = -1
-answer = [[MAXSIZE] * m for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
+answer = [[MAXSIZE] *m for _ in range(n)]
+visited =  [[False] *m for _ in range(n)]
+#print(answer)
 
-# 목표 지점 찾기
+#목표 지점 찾기
 def find_start():
     for i in range(n):
         for j in range(m):
-            if arr[i][j] == 2:
-                return i, j
-    return None
+            if arr[i][j] ==2:
+                return i,j
+def print_arr(map_arr):
+    for row in map_arr:
+       print(' '.join(map(str,row)))
 
-# BFS 거리 찾기
-def find_distance():
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+# print(find_start())
+#print_arr(arr)
+# print_arr(answer)
+# print(visited)
+# 목표 지점 - 원하는 지점까지의 거리 구하기
+# 목표 지점으로부터 dfs를 활용해, 덱에 거리를 넣는다. 
+def find_distance(n,m):
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    def in_range(x,y):
+        return 0<=x<n and 0<=y<m
 
-    def in_range(x, y):
-        return 0 <= x < n and 0 <= y < m
+    target_x,target_y = find_start() #목표지점
+    #print(target_x,target_y,'++')
 
-    start = find_start()
-    if not start:
-        return  # 시작 지점이 없을 경우
+    visited[target_x][target_y] = True
+    que = deque([(target_x,target_y)])
+    answer[target_x][target_y] = 0
 
-    sx, sy = start
-    queue = deque([(sx, sy)])
-    answer[sx][sy] = 0
-    visited[sx][sy] = True
-
-    while queue:
-        x, y = queue.popleft()
-
+    while (que):
+        #print(que)
+        x,y= que.popleft()
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if in_range(nx, ny) and not visited[nx][ny] and arr[nx][ny] == 1:
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if in_range(nx,ny) and not visited[nx][ny] and arr[nx][ny] != 0 :
+                answer[nx][ny] = answer[x][y]+1
                 visited[nx][ny] = True
-                answer[nx][ny] = answer[x][y] + 1
-                queue.append((nx, ny))
-
-# 모든 0은 0으로 초기화
+                que.append([nx,ny])
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 0:
             answer[i][j] = 0
-
-find_distance()
-
-# 출력
-for row in answer:
-    print(' '.join(map(str, row)))
+find_distance(n,m)
+print_arr(answer)
