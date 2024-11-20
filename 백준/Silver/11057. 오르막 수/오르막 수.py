acc9b31dfeea6d517d3f61a8ddb5bc30daaa6866
김성운 <115334493+seongun1@users.noreply.github.com>
2024-11-21 01:33:13 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.py
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -2,16 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-cnt=0
 dp =list([1] *(10) for _ in range(n+1))
 #print(dp)
-if n>1:
-    for k in range(2,n+1):
-        dp[k][0] = sum(dp[k-1])
-        for i in range(1,10):
-            tmp =0
-            for j in range(i):
-                tmp += dp[k-1][j]
-            dp[k][i] = dp[k][0] - tmp
-            #print(f"{dp[k][i]} = {dp[k][0]} - {tmp}")       
-print(sum(dp[n])%10007)
+#dp[k][i] = 자릿수가 k 번째인 수에서, 마지막 자릿수가 i일때 가능한 경우의 수.
+for k in range(2,n+1):
+    dp[k][0] = dp[k-1][0]
+    for i in range(1,10):
+        dp[k][i] = (dp[k-1][i] + dp[k][i-1]) % 10007
+print(sum(dp[n]) % 10007)
