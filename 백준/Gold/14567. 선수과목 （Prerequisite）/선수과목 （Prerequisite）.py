4b8c7273759761481efc042b2b58f12fc6e821df
김성운 <115334493+seongun1@users.noreply.github.com>
2024-11-28 00:41:51 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 1884 ms, Memory: 125152 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 
 n,m = map(int,input().split())
 sub =[]
-ans = [1] *(n)
+ans = [1] *(n+1)
 for _ in range(m):
     sub.append(list(map(int,input().split())))
 sub.sort()
 for a,b in sub:
-    if ans[b-1] <= ans[a-1]:
-        ans[b-1] = ans[a-1] +1
-print(' '.join(map(str,ans)))
+    if ans[b] <= ans[a]:
+        ans[b] = ans[a] +1
+print(' '.join(map(str,ans[1:])))
