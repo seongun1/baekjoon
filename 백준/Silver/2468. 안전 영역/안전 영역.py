85003c2a25db03d4f42464f67366aec7d7c2735e
김성운 <115334493+seongun1@users.noreply.github.com>
2024-11-27 11:22:08 +0900

    [Silver I] Title: 안전 영역, Time: 856 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,60 @@
+import sys
+input= sys.stdin.readline
+from collections import deque
+n = int(input())
+arr=  [list(map(int,input().split())) for _ in range(n)]
+
+def find_max():
+    max_val = 0
+    for a in arr:
+        tmp = max(a)
+        max_val = max(tmp,max_val)
+    return max_val
+def print_arr(arr):
+    for a in arr:
+        for i in a:
+            print(i,end=' ')
+        print()
+max_val = find_max()
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+def safe_area(n,rained):
+    visited=[[False] *n for _ in range(n)]
+    # print_arr(rained)
+    def in_range(n,x,y):
+        return 0<=x<n and 0<=y<n
+    ans =0
+    for i in range(n):
+        for j in range(n):
+            if not rained[i][j] and not visited[i][j]: #방문하지 않은 지역과 잠기지 않은 지역
+                # print(i,j,'++++')
+                ans +=1
+                visited[i][j] = True
+                que = deque()
+                que.append((i,j))
+                while(que):
+                    x,y = que.popleft()
+                    for k in range(4):
+                        nx,ny = x+dx[k],y+dy[k]
+                        if in_range(n,nx,ny) and not visited[nx][ny] and not rained[nx][ny]:
+                            visited[nx][ny] = True
+                            que.append((nx,ny))
+    # print('___________')    
+    # print_arr(visited)
+    # print('___________')    
+    return ans
+
+
+# print(arr)
+# print(max_val)
+# print(is_rained)
+max_ans = 1
+for rain in range(1,max_val+1):
+    rained = [[False] * n for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] <= rain:
+                rained[i][j] = True
+    max_ans = max(safe_area(n,rained),max_ans)
+print(max_ans)
