1705fad2be0e990811058fcc39ec65324bcae6df
김성운 <115334493+seongun1@users.noreply.github.com>
2024-11-27 11:25:32 +0900

    [Silver I] Title: 안전 영역, Time: 680 ms, Memory: 34116 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -5,11 +5,12 @@ n = int(input())
 arr=  [list(map(int,input().split())) for _ in range(n)]
 
 def find_max():
-    max_val = 0
-    for a in arr:
-        tmp = max(a)
-        max_val = max(tmp,max_val)
-    return max_val
+    # max_val = 0
+    # for a in arr:
+    #     tmp = max(a)
+    #     max_val = max(tmp,max_val)
+    # return max_val
+    return max(map(max,arr)) #--> 간결하게 개선
 def print_arr(arr):
     for a in arr:
         for i in a:
@@ -18,11 +19,10 @@ def print_arr(arr):
 max_val = find_max()
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
-def safe_area(n,rained):
+def safe_area(rained):
+    n = len(rained)
     visited=[[False] *n for _ in range(n)]
     # print_arr(rained)
-    def in_range(n,x,y):
-        return 0<=x<n and 0<=y<n
     ans =0
     for i in range(n):
         for j in range(n):
@@ -36,7 +36,7 @@ def safe_area(n,rained):
                     x,y = que.popleft()
                     for k in range(4):
                         nx,ny = x+dx[k],y+dy[k]
-                        if in_range(n,nx,ny) and not visited[nx][ny] and not rained[nx][ny]:
+                        if  (0<= nx <n and 0<= ny <n) and not visited[nx][ny] and not rained[nx][ny]:
                             visited[nx][ny] = True
                             que.append((nx,ny))
     # print('___________')    
@@ -56,5 +56,5 @@ for rain in range(1,max_val+1):
         for j in range(n):
             if arr[i][j] <= rain:
                 rained[i][j] = True
-    max_ans = max(safe_area(n,rained),max_ans)
+    max_ans = max(safe_area(rained),max_ans)
 print(max_ans)
