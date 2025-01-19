f784e1b29f9bc6a1b9e93020f45006c77495845b
김성운 <115334493+seongun1@users.noreply.github.com>
2025-01-19 14:44:26 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 2315.57 ms, Memory: 267 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,39 +1,125 @@
-from collections import defaultdict
-import sys
-sys.setrecursionlimit(10**7)
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+from collections import deque, defaultdict
 
 def solution(edges):
-    G_out = defaultdict(list)
-    G_in = defaultdict(int)
-    visited = set()
-    shapes = {"stick": 0, 'donut': 0, 'eight': 0}
+    graph = defaultdict(list)
+    indir = defaultdict(int)
+    outdir = defaultdict(int)
+    nodes = set()
 
-    # 1. 그래프 만들기
-    for a, b in edges:
-        G_out[a].append(b)
-        G_in[b] += 1
+    for s, e in edges:
+        graph[s].append(e)
+        indir[e] += 1
+        outdir[s] += 1
+        nodes.add(s)
+        nodes.add(e)
 
-    # 2. root 찾기 (out이 2개 이상이고 in이 0개인 노드는 root 밖에 없음)
-    root = [
-        node
-        for node in G_out
-        if len(G_out[node]) >= 2 and G_in[node] == 0
-    ][0]
+    # 생성 정점
+    start = 0
+    for node in nodes:
+        if indir[node] == 0 and outdir[node] >= 2:
+            start = node
+            break
 
-    # 3. 모양 확인 함수 정의
-    def find_shape(now):
-        if now in visited:
-            return 'donut'
-        if not G_out[now]:
-            return 'stick'
-        if len(G_out[now]) == 2:
-            return 'eight'
-        visited.add(now)
-        return find_shape(G_out[now][0])
+    # 생성 정점 관련 정보들 제거
+    nodes.remove(start)
+    for node in graph[start]:
+        indir[node] -= 1
 
-    # 4. root로부터 연결된 그래프 모양 확인
-    for next in G_out[root]:
-        shape = find_shape(next)
-        shapes[shape] += 1
+    # visited 표시를 위한 bfs
+    visited = {node: False for node in nodes}
+    def bfs(x):
+        q = deque([x])
+        visited[x] = True
+        while q:
+            node = q.popleft()
+            for nnode in graph[node]:
+                if not visited[nnode]:
+                    visited[nnode] = True
+                    q.append(nnode)
+    # 모양 분류
+    donut, stick, eight = 0, 0, 0
+    for node in nodes:
+        if visited[node]:
+            continue
+        # 막대: 들어오는 간선은 없고 나가는 간선은 0개 이상
+        if indir[node] == 0 and outdir[node] >= 0:
+            bfs(node)
+            stick += 1
+            continue
 
-    return [root, shapes['donut'], shapes['stick'], shapes['eight']]
+        if indir[node] == 2 and outdir[node] == 2:
+            bfs(node)
+            eight += 1
+            continue
+    # 8자 탐색 끝나고도 in, out 이 같다면 도넛
+    for node in nodes:
+        if visited[node]:
+            continue
+        if indir[node] == outdir[node]:
+            bfs(node)
+            donut += 1
+
+    return [start, donut, stick, eight]
