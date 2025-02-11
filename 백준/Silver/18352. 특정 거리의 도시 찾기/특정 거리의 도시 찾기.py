fa76efb18ebf766dcd389b9dfca84537e9095d87
김성운 <115334493+seongun1@users.noreply.github.com>
2025-02-11 15:16:15 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1680 ms, Memory: 100836 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,32 +1,30 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
-n, m, k, x = map(int, input().split())
+n,m,k,x = map(int,input().split())
 
-# 그래프 인접 리스트 생성
-arr = [[] for _ in range(n + 1)]
+arr =[[] for _ in range(n+1)]
 for _ in range(m):
-    a, b = map(int, input().split())
+    a,b = map(int,input().split())
     arr[a].append(b)
+#print(arr)
 
-# 최단 거리 저장 리스트 (모든 도시까지의 거리 초기화)
-distance = [-1] * (n + 1)
-distance[x] = 0  # 출발 도시의 거리는 0
+que = deque()
+distance = [-1] *(n+1)
+#print(visited)
+distance[x] = 0
+que.append(x)
 
-# BFS 탐색 시작
-que = deque([x])
-while que:
+while (que):
     start = que.popleft()
-    for next_city in arr[start]:
-        if distance[next_city] == -1:  # 아직 방문하지 않은 도시라면
-            distance[next_city] = distance[start] + 1
-            que.append(next_city)
+    for i in arr[start]:
+        if distance[i] == -1:
+            distance[i] = distance[start] + 1
+            que.append(i)
 
-# 정답 출력 (거리가 정확히 k인 도시들)
-ans = [i for i in range(1, n + 1) if distance[i] == k]
-
-if ans:
-    print(*ans, sep='\n')
-else:
+ans = [i for i in range(1,n+1) if distance[i] == k]
+ans.sort()
+if not ans:
     print(-1)
+else:
+    print(*ans, sep= '\n')
