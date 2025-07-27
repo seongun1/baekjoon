0802771f2439139b7a4c1da4a60f76f4d2478961
김성운 <115334493+seongun1@users.noreply.github.com>
2025-07-27 18:05:48 +0900

    [Silver III] Title: 자리배정, Time: 588 ms, Memory: 67428 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.py
+++ 백준/Silver/10157. 자리배정/자리배정.py
@@ -1,43 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-c,r = map(int,input().split()) #가로 / 세로
-arr =[[None] * c for _ in range(r)]
-dx = [-1,0,1,0] #위 -> 오른 -> 아래 -> 왼
-dy = [0,1,0,-1]
+m,n = map(int,input().split())
 k = int(input())
-nx = [0,1,0,-1]
-ny = [1,0,-1,0]
 
-dir =0
-count =1
+arr = [[0]*m for _ in range(n)]
+#print(arr)
+dx = [-1,0,1,0] #위 오른 아래 왼
+dy = [0,1,0,-1]
 
-cur_row,cur_col = r-1 ,0
-ans_x,ans_y = 1,1
-arr[cur_row][cur_col] = (ans_x,ans_y)
+x,y = n-1,0 #왼쪽 아래부터 시작
+seat =1
+dir =0 #현재 방향
+arr[x][y] = seat
+
+if k > n*m:
+    print(0)
+    sys.exit(0)
+
+while (seat < k): #대기번호 k 인사람이 올떄까지 (대기번호 k까지 채워야함)
+    nx,ny = x + dx[dir],y+dy[dir]
+    if 0<=nx < n and 0<=ny<m and arr[nx][ny] ==0:
+        x,y = nx,ny
+    else: #범위가 넘어가면 방향을 바꾼 뒤 다시
+        dir = (dir+1) %4
+        continue
+    seat +=1
+    arr[x][y] = seat
+    #print('==',ans_x,ans_y,dir,x,y)
 
 def print_arr():
     for a in arr:
         for i in a:
-            print(i,end =' ')
+            print(i,end=' ')
         print()
-
-if r*c < k:
-    print(0)
-    exit()
-
-for _ in range(1,k):
-    next_row ,next_col = cur_row + dx[dir] , cur_col + dy[dir]
-
-    #만약 arr을 넘어가면, 방향 전환
-    if not (0<= next_row < r and  0<=next_col< c) or arr[next_row][next_col] is not None:
-        dir = (dir +1) %4
-        next_row ,next_col = cur_row + dx[dir] , cur_col + dy[dir]
-
-    arr[next_row][next_col] = (ans_x + nx[dir] , ans_y + ny[dir])
-    cur_row ,cur_col = next_row, next_col
-    ans_x += nx[dir]
-    ans_y += ny[dir]
-
 #print_arr()
-print(ans_x,ans_y)
+
+print(y+1,n-x)
