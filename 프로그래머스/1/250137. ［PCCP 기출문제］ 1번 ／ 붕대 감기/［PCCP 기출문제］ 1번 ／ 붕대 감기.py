7032a8a283864f57e1bccf262406e47a8ca2fda7
김성운 <115334493+seongun1@users.noreply.github.com>
2025-07-27 19:10:54 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.39 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,32 +1,26 @@
 def solution(bandage, health, attacks):
     answer = 0
-    max_seconds = attacks[-1][0]
-    
-    max_health = health #최대 체력
-    heal_seconds  =0 # 연속으로 힐 한 시간
-    for seconds in range(1,max_seconds+1):
-        can_heal = True
-        for a in attacks:
-            if seconds == a[0]: #몬스터가 공격하는 시간이 있다면
-                health -= a[1] # 대미지 만큼 대미지를 받는다.
-                heal_seconds =0
-                can_heal = False #힐을 하지 못하고 다음 초로 넘어간다.
-                
-                
-        if health <=0: #체력이 0 밑으로 떨어지면 게임 종료하며 return -1
+    tmp = attacks[:]
+    tmp.sort(key = lambda x : x[0],reverse = True)
+    max_time = tmp[0][0]
+    #print('max_time',max_time)
+    max_health = health
+    heal_time =0 #연속 힐 시간
+    attack_time =0 #공격 시간
+    for time in range(1,max_time+1):
+        if time == attacks[attack_time][0]: #몬스터가 공격하는 시간이라면 = 데미지 받고, 연속 힐 시간 초기화
+            health -= attacks[attack_time][1]
+            heal_time =0
+            attack_time = min(len(attacks) -1 , attack_time +1 )
+        else: #몬스터 공격 안하는 시간 = 힐 하고, 연속 힐 시간 +1
+            health = min(max_health, health + bandage[1])
+            heal_time +=1
+        if health <= 0: #만약 죽는다면 -> -1 return
             return -1
-        
-        #print(seconds,health)
-        if can_heal: #힐을 할 수 있는 시간(몬스터에게 대미지를 안받는 시간)
-            if max_health > health: #체력이 최대 체력보다 작다면 힐을 한다.
-                health += bandage[1]
-                heal_seconds +=1
-            if heal_seconds >= bandage[0]: #시전 시간을 넘어간다면, 추가 힐
-                health += bandage[2]
-                heal_seconds =0 #연속 힐 시간 초기화
-
-            if max_health < health:
-                health = max_health
-
-    
+        if heal_time >= bandage[0]: #연속 힐 시간이 시전 시간을 넘는다면 = 추가 회복량, 시전 시간 초기화
+            health = min(max_health , health + bandage[2])
+            heal_time =0
+        #print(time,health)
     return health
+            
+    return answer
