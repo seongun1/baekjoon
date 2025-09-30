77c0aa4173a07829292700edf5c7020797eb0399
김성운 <115334493+seongun1@users.noreply.github.com>
2025-10-01 00:45:56 +0900

    [Gold V] Title: LCS, Time: 432 ms, Memory: 58332 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -24,8 +24,8 @@ for i in range(1,len(s1)): #첫 열 확인
 for i in range(1,len(s1)):
     for j in range(1,len(s2)):
         if s1[i] == s2[j]:
-            dp[i][j] = max(dp[i-1][j-1] +1, dp[i-1][j] , dp[i][j-1])
+            dp[i][j] =dp[i-1][j-1] +1
         else:
-            dp[i][j] = max(dp[i][j-1], dp[i-1][j],dp[i-1][j-1])
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
 
 print(dp[-1][-1])
