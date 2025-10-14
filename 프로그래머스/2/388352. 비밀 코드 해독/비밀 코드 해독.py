05c5ce53d64ac77443f7670b8dec3ad0d1bc0b34
김성운 <115334493+seongun1@users.noreply.github.com>
2025-10-14 19:57:26 +0900

    [level 2] Title: 비밀 코드 해독, Time: 223.18 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,10 +1,19 @@
 from itertools import combinations
 
 def solution(n, q, ans):
-    candidates = list(combinations(range(1,n+1),5))
-    for arr , count in zip(q,ans):
-        candidates = [c for c in candidates if len(set(arr) & set(c)) == count]
-        # for c in candidates[:]: #삭제 시 복사본을 순회하지 않으면 remove 하고 문제가 생김
-        #     if len(set(arr) & set(c))  != count:
-        #         candidates.remove(c)
-    return len(candidates)
+    arr = [i for i in range(1,n+1)]
+    candi = list((combinations(arr,5)))
+    answer =[]
+    def filter_code (ans,q,candi):
+        for secret_code in candi:
+            flag = True
+            for i,code in enumerate(q):
+                if len((set(secret_code) & set(code))) != ans[i]:
+                    flag = False
+                    break
+            if flag:
+                answer.append(secret_code)
+                
+    filter_code(ans,q,candi)
+    
+    return len(answer)
