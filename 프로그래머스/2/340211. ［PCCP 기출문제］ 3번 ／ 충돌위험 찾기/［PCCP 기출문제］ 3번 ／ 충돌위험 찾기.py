753f64057c52a594b513dd322d55fd71adb949bd
김성운 <115334493+seongun1@users.noreply.github.com>
2025-10-15 20:49:40 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 845.75 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -1,38 +1,38 @@
 from collections import defaultdict
 
 def solution(points, routes):
-    # (t, x, y)별로 로봇이 몇 대 왔는지 기록
-    record = defaultdict(int)
-    
-    for route in routes:
-        t = 0
-        # 1) t=0, 출발 위치 기록
-        cur = points[route[0] - 1].copy()
-        record[(t, cur[0], cur[1])] += 1
-        
-        # 2) 경로의 모든 구간을 순서대로 처리
-        for i in range(len(route) - 1):
-            start = points[route[i]   - 1]
-            end   = points[route[i+1] - 1]
-            cur = start.copy()
-            
-            # 목표 지점에 도달할 때까지 한 칸씩 이동하며 기록
-            while (cur[0] != end[0]) or (cur[1] != end[1]):
-                t += 1
-                # r 좌표부터 맞추고
-                if cur[0] < end[0]:
-                    cur[0] += 1
-                elif cur[0] > end[0]:
-                    cur[0] -= 1
-                # r 좌표가 같아지면 c 좌표 이동
-                elif cur[1] < end[1]:
-                    cur[1] += 1
-                else:
-                    cur[1] -= 1
-                
-                record[(t, cur[0], cur[1])] += 1
+    """
+    points: list of [r,c] for points 1..n
+    routes: list of lists: each route is [p1, p2, ..., pm] (1-based indices)
+    반환: 문제의 정의대로 '동일 시간·동일 좌표에 2대 이상 모인 (time,coord) 횟수의 합'
+    """
+    arr = defaultdict(int)  # key = (time, r, c) -> count
 
-    # 3) 같은 시간·같은 좌표에 2대 이상 모인 이벤트 수 세기
-    #    (record[(t,x,y)] >= 2 인 key의 개수가 정답)
-    answer = sum(1 for cnt in record.values() if cnt >= 2)
+    for route in routes:
+        start = list(points[route[0]-1])  # 복사
+        time = 0
+        arr[(time, start[0], start[1])] += 1
+
+        # 각 구간(인접 포인트 쌍)마다 이동 시뮬레이션
+        for j in range(1, len(route)):
+            end = list(points[route[j]-1])
+            while start[0] != end[0] or start[1] != end[1]:
+                time += 1
+                # r 좌표 변화 우선
+                if start[0] != end[0]:
+                    if start[0] > end[0]:
+                        start[0] -= 1
+                    else:
+                        start[0] += 1
+                else:
+                    # r이 같을 때만 c를 바꾼다
+                    if start[1] > end[1]:
+                        start[1] -= 1
+                    else:
+                        start[1] += 1
+                arr[(time, start[0], start[1])] += 1
+        # 마지막 점 도착 후 로봇은 떠남 -> 이 시점 이후 기록하지 않음
+
+    # (time, coord) 당 2대 이상이면 1씩 더함
+    answer = sum(1 for cnt in arr.values() if cnt >= 2)
     return answer
